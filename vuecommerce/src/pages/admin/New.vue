ea6d8651cece36e9f02bbc429e4d779c71124e6f
Zorex Salvo <zorexsalvo@gmail.com>
2017-10-01 19:15:05 +0800

    Add validation attributes to the input elements
    Used plugin VeeValidate:
    
    npm install --save vee-validate

--- vuecommerce/src/pages/admin/New.vue
+++ vuecommerce/src/pages/admin/New.vue
@@ -5,26 +5,47 @@
       <div class="container">
         <p class="title">
           Create New Products
-        </p>
-      </div>
+        </p> </div>
     </div>
   </section>
   <br>
   <div class="container">
+
+    <form @submit.prevent="saveProduct">
     <div class="columns">
+
         <div class="column">
+
             <div class="field">
                 <label class="label">Name</label>
                 <div class="control">
-                    <input class="input" type="text" placeholder="Name" v-validate="'required'">
+                    <input :class="{'input': true, 'error': errors.has('name')}"
+                           type="text"
+                           placeholder="Name"
+                           v-validate="'required'"
+                           v-model="name"
+                           name="name">
                 </div>
+                <span v-show="errors.has('name')" class="tag">
+                    Name is required.
+                </span>
             </div>
+
             <div class="field">
                 <label class="label" for="">Price</label>
                 <div class="control">
-                    <input class="input" type="text" placeholder="Price">
+                    <input :class="{'input': true, 'error': errors.has('price')}"
+                           type="number"
+                           placeholder="Price"
+                           v-validate="'required'"
+                           v-model="price"
+                           name="price">
                 </div>
+                <span v-show="errors.has('price')" class="tag">
+                    Price is required.
+                </span>
             </div>
+
             <div class="field">
                 <label class="label" for="">Manufacturer</label>
                 <div class="control div select">
@@ -34,19 +55,70 @@
                 </div>
             </div>
         </div>
+
         <div class="column">
-        <div class="field">
-            <label class="label" for="">Image</label>
-            <div class="control">
-                <input class="input" type="text" placeholder="Image">
+            <div class="field">
+                <label class="label" for="">Image</label>
+                <div class="control">
+                    <input :class="{'input': true, 'error': errors.has('image')}"
+                           type="text"
+                           v-validate="'required'"
+                           v-model="image"
+                           placeholder="Image"
+                           name="image">
+                </div>
+                <span v-show="errors.has('image')" class="tag">
+                    Image is required.
+                </span>
+            </div>
+            <div class="field">
+                <label class="label" for="">Description</label>
+                <div class="control">
+                    <textarea :class="{'textarea': true, 'error': errors.has('description')}"
+                              v-validate="'required'"
+                              v-model="description"
+                              name="description"
+                              cols="30"
+                              rows="10"
+                              placeholder="Textarea">
+                    </textarea>
+                </div>
+                <span v-show="errors.has('description')" class="tag">
+                    Description is required.
+                </span>
             </div>
         </div>
-        <div class="field">
-            <label class="label" for="">Description</label>
-            <div class="control"><textarea id="" class="textarea" name="" cols="30" rows="10" placeholder="Textarea"></textarea></div>
-        </div>
-        </div>
+
     </div>
+
+    <input class="button" type="submit" value="Submit">
+    </form>
+
+
   </div>
   </div>
 </template>
+
+<style>
+.input.error {
+  border-color: red !important;
+}
+</style>
+
+<script>
+  export default({
+    data() {
+      return {
+        name: '',
+        price: '',
+        image: '',
+        description: '',
+      };
+    },
+    methods: {
+      saveProduct() {
+        console.log(this.errors);
+      },
+    },
+  });
+</script>
